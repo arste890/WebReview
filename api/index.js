14cfdf88d2ec9013dcbf93b21c324b5e5e50a7a0
Alec Stevens <60017004+arste890@users.noreply.github.com>
2026-01-04 18:08:01 -0700

    fix: add index.js entry point and @azure/functions dependency

---
+++ api/index.js
@@ -0,0 +1,7 @@
+// Azure Functions entry point
+// Import all function registrations
+
+require('./src/functions/auth');
+require('./src/functions/users');
+require('./src/functions/projects');
+require('./src/functions/feedback');
